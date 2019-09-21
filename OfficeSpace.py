1dee0416b3c88bea23ea58093dc8d9c45abfcaae
dumpling641 <55337724+dumpling641@users.noreply.github.com>
2019-09-21 14:48:23 -0500

    Update OfficeSpace.py

--- OfficeSpace.py
+++ OfficeSpace.py
@@ -18,8 +18,8 @@
 
 #  Date Last Modified:
 
-def total_area(w, h):
-    return w * h
+'''def total_area(w, h):
+    return w * h'''
 
 def main():
 
@@ -33,6 +33,15 @@ def main():
     lines = [lines[i * n:(i + 1) * n] for i in range((len(lines) + n - 1) // n)]
     print(lines)
 
-
-
+    area_list=[]
+    final_area=[]
+    for n in range(len(lines):
+        area_list.append((lines[n][0]).split(" "))
+    [int(i) for i in area_list]
+    for number in range(len(area_list)):
+        if number%2==0:
+            added = area_list[number]*area_list[number+1]
+            final_area.append(added)
+    print(final_area)
+        
 main()
